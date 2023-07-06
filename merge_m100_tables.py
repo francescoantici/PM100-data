cdee51eaa8cb79c315c6bbe6a149641b58379e77
francescoantici <francesco.antici98@gmail.com>
2023-07-06 16:21:09 +0900

    merge_m100_tables update

--- merge_m100_tables.py
+++ merge_m100_tables.py
@@ -2,6 +2,15 @@ import pandas as pd
 import os 
 
 def merge_job_tables(data_path:str) -> pd.DataFrame:
+    """
+    Merge the job tables of each month into a single one
+
+    Args:
+        data_path (str): Path to the parquet files containing the job table files.
+
+    Returns:
+        pd.DataFrame: The merged job table.
+    """
     
     table_path_list = [os.path.join(data_path, f) for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f)) if ".parquet" in f]
     
@@ -16,8 +25,12 @@ def merge_job_tables(data_path:str) -> pd.DataFrame:
 
 if __name__ == "__main__":
     
+    # Path to the parquet files containing the job table files
     job_table_data_path = ""
+    
+    # Path to the merged job table
+    job_table_merged_path = ""
 
-    merge_job_tables(job_table_data_path).to_parquet("job_table.parquet")
+    merge_job_tables(job_table_data_path).to_parquet(job_table_merged_path)
     
     
